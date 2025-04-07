e7f6b45b37e0df3d9f5660714f021749a09e1822
Christopher Lepski <c.lepski@sprinteins.com>
2025-04-07 11:37:41 +0200

    test: Add test

---
+++ foundation/utils/xml.spec.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable import/no-extraneous-dependencies */
+import { expect } from '@open-wc/testing';
+
+describe('Xml utility', () => {
+  describe('queryLDevice', () => {
+    it('find LDevice in xml', () => {
+      expect(true).to.equal(true);
+    });
+  });
+});
